77882f2ed348be861f38ed791e607dedf5bc05cd
araldwin <araldwin@gmail.com>
2023-07-26 10:53:20 +0000

    complete navBar

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://moments-ara-ef26b303c308.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/from-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
