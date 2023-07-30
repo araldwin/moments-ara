ccaf5bf616f968a51fe77f4008f6b47f990cc641
araldwin <araldwin@gmail.com>
2023-07-30 07:19:27 +0000

    Add edit,delete and infinite scroll to Comments component

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,13 +8,19 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment";
+
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import Comment from "../comments/Comment";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
 	const { id } = useParams();
 	const [post, setPost] = useState({ results: [] });
+
 	const currentUser = useCurrentUser();
 	const profile_image = currentUser?.profile_image;
 	const [comments, setComments] = useState({ results: [] });
@@ -54,11 +60,22 @@ function PostPage() {
 						"Comments"
 					) : null}
 					{comments.results.length ? (
-						comments.results.map((comment) => (
-							<Comment key={comment.id} {...comment} />
-						))
+						<InfiniteScroll
+							children={comments.results.map((comment) => (
+								<Comment
+									key={comment.id}
+									{...comment}
+									setPost={setPost}
+									setComments={setComments}
+								/>
+							))}
+							dataLength={comments.results.length}
+							loader={<Asset spinner />}
+							hasMore={!!comments.next}
+							next={() => fetchMoreData(comments, setComments)}
+						/>
 					) : currentUser ? (
-						<span>No comments yet, be the first to comment</span>
+						<span>No comments yet, be the first to comment!</span>
 					) : (
 						<span>No comments... yet</span>
 					)}
